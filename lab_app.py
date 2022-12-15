ee3cf033e9e5d1ca66c5a74fa18962296594a69f
Gabriel Peterson <gabrieldpeterson@gmail.com>
2022-12-14 16:41:51 -0800

    Add radio buttons that will narrow the times grabbed from the db

--- lab_app.py
+++ lab_app.py
@@ -37,13 +37,28 @@ def lab_env_db():
 
 
 def get_records():
-    from_date_str   = request.args.get('from',time.strftime('%Y-%m-%d %H:%M')) #Get the from date value from the URL
-    to_date_str     = request.args.get('to',time.strftime('%Y-%m-%d %H:%M'))   #Get the to date value from the URL
+    from_date_str = request.args.get('from',time.strftime('%Y-%m-%d %H:%M')) #Get the from date value from the URL
+    to_date_str = request.args.get('to',time.strftime('%Y-%m-%d %H:%M'))   #Get the to date value from the URL
+    range_h_form = request.args.get('range_h', '')
+
+    range_h_int = 'nan'
+
+    try:
+        range_h_int = int(range_h_form)
+    except:
+        print('range_h_form not a number')
 
     if not validate_date(from_date_str):
         from_date_str = time.strftime('%Y-%m-%d 00:00')
     if not validate_date(to_date_str):
         to_date_str = time.strftime('%Y-%m-%d %H:%M')
+
+    if isinstance(range_h_int, int):
+        time_now = datetime.datetime.now()
+        time_from = time_now - datetime.timedelta(hours = range_h_int)
+        time_to = time_now
+        from_date_str = time_from.strftime('%Y-%m-%d %H:%M')
+        to_date_str = time_to.strftime('%Y-%m-%d %H:%M')
 
     conn = sqlite3.connect('/var/www/lab_app/lab_app.db')
     curs = conn.cursor()
